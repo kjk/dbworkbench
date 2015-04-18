3cb0124273f59e3faef16544b29e1ba98f996a51
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-04-18 00:49:03 -0700

    deploy script

--- ansible/build.py
+++ ansible/build.py
@@ -12,9 +12,11 @@ src_dir = os.path.dirname(script_dir)
 assert os.path.exists(src_dir), "%s doesn't exist" % src_dir
 assert os.path.exists(pj(src_dir, "main.go")), "%s doesn't exist" % pj(src_dir, "main.go")
 
+
 def abort(s):
     print(s)
     sys.exit(1)
+
 
 def git_ensure_clean():
     out = subprocess.check_output(["git", "status", "--porcelain"])
@@ -23,10 +25,46 @@ def git_ensure_clean():
         print(out)
         sys.exit(1)
 
+
 def git_trunk_sha1():
     return subprocess.check_output(["git", "log", "-1", "--pretty=format:%H"])
+
+
+def add_dir_files(zip_file, dir, dirInZip=None):
+    if not os.path.exists(dir):
+        abort("dir '%s' doesn't exist" % dir)
+    for (path, dirs, files) in os.walk(dir):
+        for f in files:
+            p = os.path.join(path, f)
+            zipPath = None
+            if dirInZip is not None:
+                zipPath = dirInZip + p[len(dir):]
+                #print("Adding %s as %s" % (p, zipPath))
+                zip_file.write(p, zipPath)
+            else:
+                zip_file.write(p)
+
+
+def zip_files(zip_path):
+    zf = zipfile.ZipFile(zip_path, mode="w", compression=zipfile.ZIP_DEFLATED)
+    zf.write("dbworkbench_linux", "dbworkbench")
+    zf.write(pj("scripts", "server_run.sh"), "server_run.sh")
+    add_dir_files(zf, "static")
+    zf.close()
+
 
 if __name__ == "__main__":
     os.chdir(src_dir)
     git_ensure_clean()
     subprocess.check_output(["./scripts/build_linux.sh"])
+    sha1 = git_trunk_sha1()
+    zip_name = sha1 + ".zip"
+    zip_path = os.path.join(src_dir, zip_name)
+    if os.path.exists(zip_path):
+        os.remove(zip_path)
+    zip_files(zip_path)
+    os.remove("dbworkbench_linux")
+    os.chdir(my_dir)
+    if os.path.exists(zip_name):
+        os.remove(zip_name)
+    os.rename(zip_path, zip_name)
