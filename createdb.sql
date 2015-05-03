e7843d8eb2af1599001751801a15fcda3f0b9c3b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-05-02 22:03:44 -0700

    we don't need username

--- createdb.sql
+++ createdb.sql
@@ -5,7 +5,6 @@ CREATE TABLE users (
   id                  INT NOT NULL SERIAL,
   created_at          TIMESTAMP NOT NULL,
   email               VARCHAR(255) NOT NULL,
-  username            VARCHAR(255) NOT NULL,
   -- either password or google_oauth_json must be set
   password            VARCHAR(255),
   google_oauth_json   VARCHAR(2048),
