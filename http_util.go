3d1676049e65e1ed9c87ede82cd83624e106af78
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-05-07 19:23:40 -0700

    gzip json responses if possible

--- http_util.go
+++ http_util.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -18,6 +20,10 @@ func getClientIP(r *http.Request) string {
 		return strings.TrimSpace(clientIP)
 	}
 	return r.RemoteAddr
+}
+
+func acceptsGzip(r *http.Request) bool {
+	return r != nil && strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
 }
 
 func serveError(w http.ResponseWriter, r *http.Request, isJSON bool, errMsg string) {
@@ -51,15 +57,35 @@ func serveJSONError(w http.ResponseWriter, r *http.Request, errMsg interface{}) 
 	}
 }
 
-func serveJSON(w http.ResponseWriter, r *http.Request, v interface{}) error {
+func serveJSON(w http.ResponseWriter, r *http.Request, v interface{}) {
+	// https://www.maxcdn.com/blog/accept-encoding-its-vary-important/
+	// prevent caching non-gzipped version
+	w.Header().Add("Vary", "Accept-Encoding")
+	doGzip := acceptsGzip(r)
+	if doGzip {
+		w.Header().Set("Content-Encoding", "gzip")
+	}
 	writeHeader(w, 200, "application/json")
-	encoder := json.NewEncoder(w)
-	return encoder.Encode(v)
+	var gz *gzip.Writer
+	var writer io.Writer
+	writer = w
+	if doGzip {
+		gz = gzip.NewWriter(w)
+		writer = gz
+	}
+	encoder := json.NewEncoder(writer)
+	if err := encoder.Encode(v); err != nil {
+		LogErrorf("err: %s\n", err)
+	}
+	if doGzip {
+		gz.Close()
+	}
 }
 
-func serveJSONP(w http.ResponseWriter, r *http.Request, v interface{}, jsonp string) error {
+func serveJSONP(w http.ResponseWriter, r *http.Request, v interface{}, jsonp string) {
 	if jsonp == "" {
-		return serveJSON(w, r, v)
+		serveJSON(w, r, v)
+		return
 	}
 
 	writeHeader(w, 200, "application/json")
@@ -67,17 +93,18 @@ func serveJSONP(w http.ResponseWriter, r *http.Request, v interface{}, jsonp str
 	if err != nil {
 		// should never happen
 		LogErrorf("json.MarshalIndent() failed with %q\n", err)
-		return err
+		return
 	}
 	res := []byte(jsonp)
 	res = append(res, '(')
 	res = append(res, b...)
 	res = append(res, ')')
-	_, err = w.Write(res)
-	return err
+	if _, err = w.Write(res); err != nil {
+		LogErrorf("err: '%s'\n", err)
+	}
 }
 
-func servePlainText(w http.ResponseWriter, r *http.Request, code int, format string, args ...interface{}) error {
+func servePlainText(w http.ResponseWriter, r *http.Request, code int, format string, args ...interface{}) {
 	writeHeader(w, code, "text/plain")
 	var err error
 	if len(args) > 0 {
@@ -85,7 +112,9 @@ func servePlainText(w http.ResponseWriter, r *http.Request, code int, format str
 	} else {
 		_, err = w.Write([]byte(format))
 	}
-	return err
+	if err != nil {
+		LogErrorf("err: '%s'\n", err)
+	}
 }
 
 func serveData(w http.ResponseWriter, r *http.Request, code int, contentType string, data []byte) {
