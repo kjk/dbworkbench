6af79ad8afa7c3846cea24a0624eb50b9d58a838
thellimist <mufuyil@gmail.com>
2015-11-27 14:32:26 -0800

    Removed and readded mac-client

---
+++ mac-client/dbworkbench/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dbworkbench
+//
+//  Created by Furkan Yilmaz on 26/11/15.
+//  Copyright © 2015 Furkan Yilmaz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
