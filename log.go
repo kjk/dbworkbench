a9b5b3694dba023648725cfe44f38c94de187a1b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-11-27 20:54:12 -0800

    move content of logrotate.go into log.go

--- log.go
+++ log.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/url"
+	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 	"sync/atomic"
 )
 
@@ -17,8 +20,9 @@ var (
 	dot               = []byte(".")
 	centerDot         = []byte("·")
 	logVerbosityLevel int32
-)
 
+	logToStdout = false
+)
 
 // the idea of verbose logging is to provide a way to turn detailed logging
 // on a per-request basis. This is an approximate solution: since there is
@@ -67,7 +71,6 @@ func OpenLogMust(fileName string, logPtr **LogFile) {
 	}
 	*logPtr = logTmp
 }
-
 
 // OpenLogFiles open error and info log files
 func OpenLogFiles() {
@@ -154,4 +157,87 @@ func LogVerbosef(format string, arg ...interface{}) {
 		s = functionFromPc(pc) + ": " + s
 	}
 	logInfo.Print(s)
+}
+
+// LogFile describes a log file
+type LogFile struct {
+	sync.Mutex
+	path      string
+	file      *os.File
+	w         io.Writer
+	useLogger bool // setting this will log like log package: with date and time
+	logger    *log.Logger
+}
+
+func (l *LogFile) dirAndBaseName() (string, string) {
+	dir := filepath.Dir(l.path)
+	base := filepath.Base(l.path)
+	return dir, base
+}
+
+func (l *LogFile) close() {
+	if l.file != nil {
+		l.file.Close()
+		l.file = nil
+	}
+}
+
+func (l *LogFile) open() (err error) {
+	flag := os.O_CREATE | os.O_APPEND | os.O_WRONLY
+	l.file, err = os.OpenFile(l.path, flag, 0644)
+	if err != nil {
+		return err
+	}
+	_, err = l.file.Stat()
+	if err != nil {
+		l.file.Close()
+		return err
+	}
+	l.w = l.file
+	if l.useLogger {
+		l.logger = log.New(l.w, "", log.LstdFlags)
+	}
+	return err
+}
+
+// NewLogFile creates LogRotate
+func NewLogFile(path string) (*LogFile, error) {
+	res := &LogFile{
+		path:      path,
+		useLogger: true,
+	}
+	if err := res.open(); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// Close closes a log file
+func (l *LogFile) Close() {
+	if l == nil {
+		return
+	}
+	l.Lock()
+	l.close()
+	l.Unlock()
+}
+
+// Print writes to log file
+func (l *LogFile) Print(s string) {
+	if logToStdout {
+		fmt.Print(s)
+	}
+	if l == nil {
+		return
+	}
+	if l.logger != nil {
+		l.logger.Print(s)
+	} else {
+		l.w.Write([]byte(s))
+	}
+}
+
+// Printf writes to log file
+func (l *LogFile) Printf(format string, arg ...interface{}) {
+	l.Print(fmt.Sprintf(format, arg...))
 }
