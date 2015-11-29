f7d6a7ae10fb424ed95557244b359da9a04ab63e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-11-28 19:25:59 -0800

    run go vet first

--- scripts/buildwin.bat
+++ scripts/buildwin.bat
@@ -1,13 +1,13 @@
 @echo on
 
+godep go vet github.com/kjk/dbworkbench
+@IF ERRORLEVEL 1 goto Error
+
 go run tools\build\main.go tools\build\util.go tools\build\cmd.go tools\build\s3.go tools\build\win.go  -no-clean-check
 @IF ERRORLEVEL 1 goto Error
 @goto EndOk
 
 call ./node_modules/.bin/gulp default
-@IF ERRORLEVEL 1 goto Error
-
-godep go vet github.com/kjk/dbworkbench
 @IF ERRORLEVEL 1 goto Error
 
 go run scripts\build_release.go
@@ -18,7 +18,7 @@ godep go build -o dbworkbench.exe
 
 go run tools\build\main.go tools\build\util.go tools\build\cmd.go tools\build\s3.go tools\build\win.go -no-clean-check
 @IF ERRORLEVEL 1 goto Error
-	
+
 goto EndOk
 
 :Error
