77d79701ef08b58adc8ff1613aaea01086d92649
thellimist <mufuyil@gmail.com>
2015-11-29 11:44:28 -0800

    + Mac: version check added

---
+++ scripts/buildmac.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+godep go vet github.com/kjk/dbworkbench
+
+# TODO: Should I do tests?
+
+./node_modules/.bin/gulp default
+
+go run scripts/build_release.go
+
+godep go build -o dbworkbench
+
+go run tools/build/main.go tools/build/util.go tools/build/cmd.go tools/build/s3.go tools/build/win.go -no-clean-check
