8fe3cb1d6ef3282be3b15ddbb78ea5d2514745f2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-11-28 17:04:48 -0800

    progress on buildwin

--- tools/buildwin/main.go
+++ tools/buildwin/main.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	flgNoCleanCheck bool
+	flgUpload bool
+)
 
 func finalizeThings(crashed bool) {
 	// nothing for us
 }
 
+func parseCmdLine() {
+	// -no-clean-check is useful when testing changes to this build script
+	flag.BoolVar(&flgNoCleanCheck, "no-clean-check", false, "allow running if repo has changes (for testing build script)")
+	flag.Parse()
+}
+
+func isGitCleanMust() bool {
+	out, err := runExe("git", "status", "--porcelain")
+	fataliferr(err)
+	s := strings.TrimSpace(string(out))
+	return len(s) == 0
+}
+
+func verifyGitCleanMust() {
+	if !flgUpload || flgNoCleanCheck {
+		return
+	}
+	fatalif(!isGitCleanMust(), "git has unsaved changes\n")
+}
+
 func main() {
 	fmt.Printf("Starting windows build\n")
+	parseCmdLine()
 }
