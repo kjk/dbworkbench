382e7e9c65be1c8cf14e0d7ef94e0ae885f029e1
thellimist <mufuyil@gmail.com>
2015-12-02 23:40:32 -0800

    bookmarks.go implemented

--- bookmarks.go
+++ bookmarks.go
@@ -1,10 +1,14 @@
 package main
 
-import "path/filepath"
+import (
+ 	"encoding/json"
+    "fmt"
+    "os"
+    "io/ioutil"
+    "path/filepath"
+)
 
-/*
-TODO: implement.
-*/
+// TODO encript password
 
 // Bookmark defines info about a database
 type Bookmark struct {
@@ -23,5 +27,76 @@ func bookmarksFilePath() string {
 
 func readAllBookmarks() (map[string]Bookmark, error) {
 	res := map[string]Bookmark{}
+
+	file, err := ioutil.ReadFile(bookmarksFilePath())
+    if err != nil {
+        fmt.Printf("File error: %v\n", err)
+        return res, err
+    }
+
+    err = json.Unmarshal(file, &res)
+    if err != nil {
+        fmt.Printf("JSON Parse error: %v\n", err)
+    	return res, err
+    }
+
 	return res, nil
+}
+
+func addBookmark(bookmark Bookmark) (map[string]Bookmark, error) {
+    res := map[string]Bookmark{}
+
+    var file *os.File
+    var err error
+    if _, err := os.Stat(bookmarksFilePath()); os.IsNotExist(err) {
+        // Path does not exist
+        file, err = os.Create(bookmarksFilePath()) // Maybe use os.NewFile?
+        if err != nil {
+            fmt.Println(err)
+        }
+        defer file.Close()
+    } else {
+        // Path exist
+        res, err = readAllBookmarks()
+        if err != nil {
+            return res, err
+        }
+    }
+
+    fmt.Println(bookmarksFilePath())
+
+    res[bookmark.Database] = bookmark
+
+    b, err := json.MarshalIndent(res, "", "  ")
+    if err != nil {
+     	return res, err
+ 	}
+
+    ioutil.WriteFile(bookmarksFilePath(), b, 0644)
+    return res, nil
+}
+
+func removeBookmark(databaseName string) (map[string]Bookmark, error) {
+    res := map[string]Bookmark{}
+
+    if _, err := os.Stat(bookmarksFilePath()); os.IsNotExist(err) {
+        // Path does not exist
+        // TODO: maybe a check?
+        return res, err
+    } else {
+        // Path exist
+        res, err = readAllBookmarks()
+        if err != nil {
+            return res, err
+        }
+    }
+
+    delete(res, databaseName)
+    b, err := json.MarshalIndent(res, "", "  ")
+    if err != nil {
+    	return res, err
+    }
+
+    ioutil.WriteFile(bookmarksFilePath(), b, 0644)
+    return res, nil
 }
