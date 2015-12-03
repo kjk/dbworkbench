c538bfcff291ac3d15c3d1144bdf47cfed68ad07
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-03 10:43:38 -0800

    gofmt bookmarks.go

--- bookmarks.go
+++ bookmarks.go
@@ -1,11 +1,11 @@
 package main
 
 import (
- 	"encoding/json"
-    "fmt"
-    "os"
-    "io/ioutil"
-    "path/filepath"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 // TODO encript password
@@ -26,81 +26,80 @@ func bookmarksFilePath() string {
 }
 
 func ifNotCreateBookmarksFile() error {
-    if _, err := os.Stat(bookmarksFilePath()); os.IsNotExist(err) {
-        // Path does not exist
-        file, err := os.Create(bookmarksFilePath()) // Maybe use os.NewFile?
-        if err != nil {
-            fmt.Println(err)
-            return err
-        }
-        defer file.Close()
-    }
+	if _, err := os.Stat(bookmarksFilePath()); os.IsNotExist(err) {
+		// Path does not exist
+		file, err := os.Create(bookmarksFilePath()) // Maybe use os.NewFile?
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		defer file.Close()
+	}
 
-    return nil
+	return nil
 }
 
 func readAllBookmarks() (map[string]Bookmark, error) {
-    ifNotCreateBookmarksFile()
+	ifNotCreateBookmarksFile()
 
-    res := map[string]Bookmark{}
+	res := map[string]Bookmark{}
 
 	fileData, err := ioutil.ReadFile(bookmarksFilePath())
-    if err != nil {
-        fmt.Printf("File error: %v\n", err)
-        return res, err
-    }
+	if err != nil {
+		fmt.Printf("File error: %v\n", err)
+		return res, err
+	}
 
-    err = json.Unmarshal(fileData, &res)
-    if err != nil {
-        fmt.Printf("JSON Parse error: %v\n", err)
-    	return res, err
-    }
+	err = json.Unmarshal(fileData, &res)
+	if err != nil {
+		fmt.Printf("JSON Parse error: %v\n", err)
+		return res, err
+	}
 
 	return res, nil
 }
 
 func addBookmark(bookmark Bookmark) (map[string]Bookmark, error) {
-    ifNotCreateBookmarksFile()
+	ifNotCreateBookmarksFile()
 
-    res := map[string]Bookmark{}
+	res := map[string]Bookmark{}
 
+	// Path exist
+	res, err := readAllBookmarks()
+	if err != nil {
+		// If the file is empty this will send an error so ignore
+		fmt.Printf("readAllBookmarks error: %v\n", err)
+		// return res, err
+	}
 
-    // Path exist
-    res, err := readAllBookmarks()
-    if err != nil {
-        // If the file is empty this will send an error so ignore
-        fmt.Printf("readAllBookmarks error: %v\n", err)
-        // return res, err
-    }
+	res[bookmark.Database] = bookmark
 
-    res[bookmark.Database] = bookmark
+	b, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return res, err
+	}
 
-    b, err := json.MarshalIndent(res, "", "  ")
-    if err != nil {
-     	return res, err
- 	}
-
-    ioutil.WriteFile(bookmarksFilePath(), b, 0644)
-    return res, nil
+	ioutil.WriteFile(bookmarksFilePath(), b, 0644)
+	return res, nil
 }
 
 func removeBookmark(databaseName string) (map[string]Bookmark, error) {
-    ifNotCreateBookmarksFile()
+	ifNotCreateBookmarksFile()
 
-    res := map[string]Bookmark{}
+	res := map[string]Bookmark{}
 
-    // Path exist
-    res, err := readAllBookmarks()
-    if err != nil {
-        return res, err
-    }
+	// Path exist
+	res, err := readAllBookmarks()
+	if err != nil {
+		return res, err
+	}
 
-    delete(res, databaseName)
-    b, err := json.MarshalIndent(res, "", "  ")
-    if err != nil {
-    	return res, err
-    }
+	delete(res, databaseName)
+	b, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		return res, err
+	}
 
-    ioutil.WriteFile(bookmarksFilePath(), b, 0644)
-    return res, nil
+	ioutil.WriteFile(bookmarksFilePath(), b, 0644)
+	return res, nil
 }
