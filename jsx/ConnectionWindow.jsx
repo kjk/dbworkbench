56a92c3daf2501b1714834ba7a00dbfb6766677f
thellimist <mufuyil@gmail.com>
2015-12-02 23:43:20 -0800

    ConnectionWindow.jsx redesigned

--- jsx/ConnectionWindow.jsx
+++ jsx/ConnectionWindow.jsx
@@ -2,110 +2,320 @@
 'use strict';
 
 var api = require('./api.js');
+var action = require('./action.js');
+var _ = require('underscore');
+
+var initName = "Unnamed Connection";
 
 var ConnectionWindow = React.createClass({
 
   getInitialState: function() {
+    // TODO: need a solution for this not making apicall sync
+    var bookmarks = {}
+    api.getBookmarks(function(data) {
+      console.log("getBookmarks: ", data);
+      bookmarks = data;
+    });
+
+    var activeBookmark = ""
+    if (bookmarks != undefined) {
+      activeBookmark = Object.keys(bookmarks)[0];
+    }
+
     return {
       connectionErrorMessage: "",
       isConnecting: false,
 
-      standardHost: "",
-      standardUsername: "",
-      standardPassword: "",
-      standardDatabase: "",
-      standardPort: "",
-      standardSSL: "require",
+      bookmarks: bookmarks,
+      activeBookmark: activeBookmark,
     };
   },
 
-  renderBookMarks: function() {
+  addBookmark: function(e) {
+
+    var bookmarkLimit = 10;
+    if (Object.keys(this.state.bookmarks).length >= bookmarkLimit) {
+      action.alertBar("Max Connection Limit is " + bookmarkLimit);
+      return;
+    }
+
+    var possibleNames = [];
+    possibleNames.push(initName);
+    for (var i = 1; i <= bookmarkLimit; i++) {
+      possibleNames.push(initName + " " + String(i));
+    };
+
+    var bookmarkTitles = [];
+    for (var key in this.state.bookmarks) {
+      bookmarkTitles.push(key);
+    }
+
+    var usedNames = _.intersection(possibleNames, bookmarkTitles);
+    var notUsedNames = _.difference(possibleNames, usedNames);
+
+    if (notUsedNames.length > 0 ) {
+      var newName = notUsedNames[0];
+    } else {
+      console.log("Error: this should not be possible");
+      return;
+    }
+
+
+    console.log("new bookmark name" + newName);
+
+    var initialBookmark = {url: "", host: "", database: newName, user: "", password: "", port: "" ,ssl: ""}
+
+    var self = this;
+    api.addBookmark(initialBookmark, function(data) {
+      console.log("bookmark added: ", data);
+      self.setState({
+        bookmarks: data,
+      });
+    });
+  },
+
+  getBookmark: function(e) {
+    var self = this;
+    api.getBookmarks(function(data) {
+      console.log("getBookmarks: ", data);
+
+      self.setState({
+        bookmarks: data,
+      });
+    });
+  },
+
+  deleteBookmark: function(e) {
+    e.stopPropagation();
+    var self = this;
+    var dbName = event.target.attributes["id"].value;
+    // console.log(dbName)
+    // console.log(event.target.attributes["id"])
+    api.removeBookmark(dbName, function(data) {
+      console.log("removeBookmarks: ", data);
+
+      if (data != undefined) {
+        var activeBookmark = Object.keys(data)[0];
+      }
+
+      self.setState({
+        bookmarks: data,
+        activeBookmark: activeBookmark,
+      });
+    });
+  },
+
+  selectBookmark: function(e) {
+    console.log("selectBookmark");
+
+    this.setState({
+      activeBookmark: event.target.attributes["id"].value,
+    });
+  },
+
+  handleFormChanged: function(name, e) {
+    console.log("handleFormChanged: ", e.target.value);
+
+    var change = this.state.bookmarks;
+    change[this.state.activeBookmark][name] = e.target.value;
+
+    this.setState({
+      bookmarks: change,
+      connectionErrorMessage: "",
+    });
+  },
+
+  handleConnect: function(e) {
+    e.preventDefault();
+    console.log("handleConnect");
+
+    var host = this.state.bookmarks[this.state.activeBookmark]["host"];
+    var port = this.state.bookmarks[this.state.activeBookmark]["port"];
+    var user = this.state.bookmarks[this.state.activeBookmark]["user"];
+    var pass = this.state.bookmarks[this.state.activeBookmark]["password"];
+    var db = this.state.bookmarks[this.state.activeBookmark]["database"];
+    var ssl = "disable";
+
+    if (port.length == 0) {
+      port = "5432";
+    }
+
+    var url = "postgres://" + user + ":" + pass + "@" + host + ":" + port + "/" + db + "?sslmode=" + ssl;
+
+    console.log("URL:" + url);
+    var self = this;
+    this.setState({
+      isConnecting: true,
+    });
+    api.connect(url, function(resp) {
+      if (resp.error) {
+        console.log("handleConnect: resp.error: ", resp.error);
+
+        self.setState({
+          connectionErrorMessage: resp.error,
+          isConnecting: false,
+        });
+
+      } else {
+        console.log("did connect");
+
+        api.addBookmark(self.state.bookmarks[self.state.activeBookmark], function(data) {
+          console.log("bookmark saved: ", data);
+        });
+
+        var connId = resp.ConnectionID;
+        var connStr = url;
+        var databaseName = resp.CurrentDatabase;
+        self.props.onDidConnect(connStr, connId, databaseName);
+
+      }
+    });
+  },
+
+  handleCancel: function(e) {
+    e.preventDefault();
+    console.log("handleCancel");
+  },
+
+  renderError: function(errorText) {
     return (
-      <div className="list-group list-special" >
+      <div className="alert alert-danger">{errorText}</div>
+    );
+  },
+
+  renderBookMarks: function() {
+    var bookmarks = [];
+    for (var key in this.state.bookmarks) {
+      var databaseName = this.state.bookmarks[key]["database"];
+
+      if (Object.keys(this.state.bookmarks).length > 1) {
+        var removeButton = <i id={key} onClick={this.deleteBookmark} className="fa fa-times pull-right"></i>;
+      }
+
+      bookmarks.push(
+        <a id={key} href="#" className="list-group-item" onClick={this.selectBookmark}>
+          <em id={key}>{databaseName}</em>
+          {removeButton}
+        </a>
+      );
+    }
+
+    return (
+      <div className="list-group list-special">
         <a href="#" className="list-group-item">
           Saved Connections
-          <span class="badge"><i className="fa fa-plus pull-right"></i></span>
+          <i id={key} onClick={this.addBookmark} className="fa fa-plus pull-right"></i>;
         </a>
 
         <hr/>
 
-        <a href="#" className="list-group-item active">
-          <em>First Connection</em>
-          <span class="badge"><i className="fa fa-times pull-right"></i></span>
-        </a>
-        <a href="#" className="list-group-item">
-          <em>Second Connection</em>
-          <span class="badge"><i className="fa fa-times pull-right"></i></span>
-        </a>
-        <a href="#" className="list-group-item">
-          <em>Third Connection</em>
-          <span class="badge"><i className="fa fa-times pull-right"></i></span>
-        </a>
+        {bookmarks}
       </div>
 
     );
   },
 
-  renderForm: function() {
-    var error;
+  renderFormElements: function() {
+    var formData = _.clone(this.state.bookmarks[this.state.activeBookmark])
+    if (formData["database"].startsWith(initName)) {
+      formData["database"] = "";
+    }
+
     if (this.state.connectionErrorMessage !== "") {
-      error = this.renderError(this.state.connectionErrorMessage);
+      var error = this.renderError(this.state.connectionErrorMessage);
     }
 
 
-    // <div className="col-md-4">
-    //   <div className="form-group center">
-    //     <div className="checkbox" name="rememberme">
-    //       <label><input type="checkbox" value="" checked="checked"/>Remember Connection</label>
-    //     </div>
-    //   </div>
-    // </div>
-
     return (
-      <form role="form">
-
-          <div className="col-md-8">
-            <div className="form-group">
-              <label className="control-label" htmlFor="db_hostname">Hostname</label>
-              <input type="text" id="db_hostname" className="form-control input-sm" />
-            </div>
+      <div>
+        <div className="col-md-8">
+          <div className="form-group">
+            <label className="control-label" htmlFor="db_hostname">Hostname</label>
+            <input
+              type="text"
+              id="db_hostname"
+              className="form-control input-sm"
+              value = {formData["host"]}
+              onChange={this.handleFormChanged.bind(this, 'host')}/>
           </div>
+        </div>
 
-          <div className="col-md-4">
-            <div className="form-group">
-              <label className="control-label" htmlFor="db_port">Port</label>
-              <input type="text" id="db_port" className="form-control input-sm" placeholder="5432"/>
-            </div>
+        <div className="col-md-4">
+          <div className="form-group">
+            <label className="control-label" htmlFor="db_port">Port</label>
+            <input
+              type="text"
+              id="db_port"
+              className="form-control input-sm"
+              value = {formData["port"]}
+              onChange={this.handleFormChanged.bind(this, 'port')} placeholder="5432"/>
           </div>
+        </div>
 
-          <div className="col-md-12">
-            <div className="form-group">
-              <label className="control-label" htmlFor="db_database">Database</label>
-              <input type="text" id="db_database" className="form-control input-sm" />
-            </div>
+        <div className="col-md-12">
+          <div className="form-group">
+            <label className="control-label" htmlFor="db_database">Database</label>
+            <input
+              type="text"
+              id="db_database"
+              className="form-control input-sm"
+              value = {formData["database"]}
+              onChange={this.handleFormChanged.bind(this, 'database')} />
           </div>
+        </div>
 
-          <div className="col-md-6">
-            <div className="form-group">
-              <label className="control-label" htmlFor="db_user">User</label>
-              <input type="text" id="db_user" className="form-control input-sm" />
-            </div>
+        <div className="col-md-6">
+          <div className="form-group">
+            <label className="control-label" htmlFor="db_user">User</label>
+            <input
+              type="text"
+              id="db_user"
+              className="form-control input-sm"
+              value = {formData["user"]}
+              onChange={this.handleFormChanged.bind(this, 'user')} />
           </div>
+        </div>
 
-          <div className="col-md-6">
-            <div className="form-group">
-              <label className="control-label" htmlFor="db_pass">Password</label>
-              <input type="text" id="db_pass" className="form-control input-sm" />
-            </div>
+        <div className="col-md-6">
+          <div className="form-group">
+            <label className="control-label" htmlFor="db_pass">Password</label>
+            <input
+              type="password"
+              id="db_pass"
+              className="form-control input-sm"
+              value = {formData["password"]}
+              onChange={this.handleFormChanged.bind(this, 'password')} />
           </div>
+        </div>
+
         {error}
 
         <div className="col-md-12">
           <div className="form-group">
-            <button className="btn btn-sm btn-primary small">Connect</button>
-            <button type="reset" id="close_connection_window" className="btn btn-block btn-default input-sm">Cancel</button>
+            <button disabled={this.state.isConnecting} onClick={this.handleConnect} className="btn btn-block btn-primary small">Connect</button>
+            <button onClick={this.handleCancel} type="reset" id="close_connection_window" className="btn btn-block btn-default small">Cancel</button>
           </div>
         </div>
+      </div>
+    );
+  },
+
+  renderForm: function() {
+    if (this.state.activeBookmark != "") {
+      var formElements = this.renderFormElements()
+    } else {
+      var formElements = (
+        <div className="col-md-12 text-center">
+            <img class="img-responsive center-block" src="/s/img/icon.png" alt=""/>
+            <h4>Please select a connection</h4>
+        </div>
+      );
+    }
+
+
+    return (
+      <form role="form">
+        {formElements}
       </form>
     );
   },
