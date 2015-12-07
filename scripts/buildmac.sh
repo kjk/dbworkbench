e9df7f4f3669cf0d57795f5aa82229dcae35b3e0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-06 16:32:20 -0800

    build: codesign mac app

--- scripts/buildmac.sh
+++ scripts/buildmac.sh
@@ -4,14 +4,24 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+
+rm -rf mac/build
+
 godep go vet github.com/kjk/dbworkbench
 
 ./node_modules/.bin/gulp default
 
+echo "generating resources .zip file..."
 go run tools/build/*.go -gen-resources
 
+echo "building dbworkbench.exe..."
 godep go build -tags embeded_resources -o mac/dbworkbench.exe
 
+echo "running xcode..."
 xcodebuild -parallelizeTargets -project mac/dbworkbench.xcodeproj/
+
+codesign --force --deep --verbose -s "Developer ID Application: Krzysztof Kowalczyk (2LGSCEWRR9)" -f "mac/build/Release/Database Workbench.app"
+
+codesign --verify --verbose "mac/build/Release/Database Workbench.app"
 
 go run tools/build/*.go
