cb70030cd1b13786a6ec925187681853f677f868
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-06 18:39:33 -0800

    mac: failed attempt at killing backend at startup if it's already running

--- mac/dbworkbench/ServerController.swift
+++ mac/dbworkbench/ServerController.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import AppKit
 
 var serverTask = NSTask()
 var waitsForMoreServerOutput = true
@@ -6,15 +7,48 @@ var waitsForMoreServerOutput = true
 // TODO: use NSTak termination handler to get notified the backend process
 // exists and show some error (or try to restart automatically)
 
+// TODO: this doesn't work because NSWorkspace.runningApplications doesn't
+// include backend process
+// I found https://github.com/beltex/SystemKit/blob/master/SystemKit/Process.swift but
+// it requires being root
+// http://stackoverflow.com/questions/2518160/programmatically-check-if-a-process-is-running-on-mac
+// is a C implementation
+// terminate backend if it's running. This can happen e.g. when app crashes
+// and doesn't terminate backend properly
+func killBackendIfRunning(backendPath : String) {
+    let wsk = NSWorkspace.sharedWorkspace()
+    let processes = wsk.runningApplications
+    for proc in processes {
+        if let appUrl = proc.executableURL {
+            if let path = appUrl.path {
+                NSLog("path: \(path)")
+                if path == backendPath {
+                    let pid = proc.processIdentifier
+                    NSLog("killing process \(pid) '\(path)' because bacckend shouldn't be running")
+                    proc.forceTerminate()
+                    // wait up to 10 secs for process to terminate
+                    var i = 10
+                    while i > 0 {
+                        if proc.terminated {
+                            return
+                        }
+                        sleep(1)
+                        i -= 1
+                    }
+                }
+            }
+        }
+    }
+}
+
 func runServer(view : ViewController) {
     // TODO: this should not be necessary but without it serverTask is nil
     serverTask = NSTask()
     let resPath = NSBundle.mainBundle().resourcePath
     let serverGoExePath = resPath! + "/dbworkbench.exe"
-    // TODO: should check if there are processes with this path already
-    // running and kill them (in case there's a left-over process from
-    // previous run)
     
+    killBackendIfRunning(serverGoExePath)
+
     serverTask.launchPath = serverGoExePath
     serverTask.currentDirectoryPath = resPath!
     //        serverTask.arguments = ["-dev"]
