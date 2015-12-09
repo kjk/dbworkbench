4c5ca1ed278fec4ab12c053b59a8812674e34528
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-08 16:46:17 -0800

    mac: move logging functions to Log.swift

--- mac/dbworkbench/ServerController.swift
+++ mac/dbworkbench/ServerController.swift
@@ -4,63 +4,6 @@ import AppKit
 var serverTask = NSTask()
 var waitsForMoreServerOutput = true
 
-// TODO: move to log.swift
-var shouldOpenLog = true
-var logLock = NSLock()
-var logFile : NSOutputStream?
-
-func openLogFileIfNeeded() {
-    if logFile != nil {
-        return
-    }
-    if !shouldOpenLog {
-        return
-    }
-
-    let dir =  NSString.pathWithComponents([getDataDir(), "log"])
-    
-    let dateFmt = NSDateFormatter()
-    dateFmt.dateFormat = "'log-'yy-MM-dd'-mac.txt"
-    let logName = dateFmt.stringFromDate(NSDate())
-    if (!NSFileManager.defaultManager().fileExistsAtPath(dir)) {
-        do {
-            try NSFileManager.defaultManager().createDirectoryAtPath(dir, withIntermediateDirectories: true, attributes: nil)
-        } catch _ {
-            // failed
-            shouldOpenLog = false
-            return
-        }
-        
-    }
-    
-    let logPath = NSString.pathWithComponents([dir, logName])
-    logFile = NSOutputStream(toFileAtPath: logPath, append: true)
-    logFile?.open()
-}
-
-func closeLogFile() {
-    logLock.lock()
-    logFile?.close()
-    logFile = nil
-    logLock.unlock()
-}
-
-func log(s : String) {
-    logLock.lock()
-    openLogFileIfNeeded()
-    print(s)
-    if let lf = logFile {
-        // TODO: only if doesn't end with '\n' already
-        let s2 = s + "\n"
-        let encodedDataArray = [UInt8](s2.utf8)
-        let n = lf.write(encodedDataArray, maxLength: encodedDataArray.count)
-        if n == -1 && false {
-            print("write failed with error: '\(logFile?.streamError)'")
-        }
-    }
-    logLock.unlock()
-}
-
 // TODO: use NSTak termination handler to get notified the backend process
 // exists and show some error (or try to restart automatically)
 
