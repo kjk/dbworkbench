2e7be37245c9a8ff7df9f6028622c87447b9c087
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-09 22:17:32 -0800

    mac: shorter way to parse output of ps -ax

--- parseps.swift
+++ parseps.swift
@@ -7,70 +7,21 @@ struct ProcInfo {
 	var pid: Int
 }
 
-func trimEmptyStringsLeft(var a : [String]) -> [String] {
-	while !a.isEmpty {
-		if a[0] == "" {
-			a.removeAtIndex(0)
-		} else {
-			return a
-		}
-	}
-	return a
-}
-
-func removeEmptyStrings(var a : [String]) -> [String] {
-	var res = [String]()
-	for var i = 0; i < a.count; i++ {
-		let el = a[i]
-		if el != "" {
-			res.append(el)
-		}
-	}
-	return res
-}
-
 // parses output of ps -ax line, which is in the format:
 // "  454 ttys000    0:00.05 -bash"
 func parsePsLine(s : String) -> ProcInfo? {
-	var parts = s.componentsSeparatedByString(" ")
-	parts = trimEmptyStringsLeft(parts)
-
-	//print(s)
-	// this should be pid
-	if parts.isEmpty {
+    var parts = s.characters.split(" ", maxSplit: 3, allowEmptySlices: false).map(String.init)
+	if parts.count != 4 {
+		print("failed to parse: '\(s)'")
 		return nil
 	}
+
 	let pidStr = parts[0]
 	guard let pid = Int(pidStr) else {
 		print("failed to convert '\(pidStr)' to int")
 		return nil
 	}
-	parts.removeAtIndex(0)
-
-	parts = trimEmptyStringsLeft(parts)
-	if parts.isEmpty {
-		return nil
-	}
-
-	//let user = parts[0]
-	parts.removeAtIndex(0)
-
-	parts = trimEmptyStringsLeft(parts)
-	if parts.isEmpty {
-		return nil
-	}
-
-	//let time = parts[0]
-	parts.removeAtIndex(0)
-
-	parts = trimEmptyStringsLeft(parts)
-	if parts.isEmpty {
-		return nil
-	}
-
-	// the rest is process name and arguments
-	parts = removeEmptyStrings(parts)
-	let name = parts.joinWithSeparator(" ")
+	let name = parts[3]
 	return ProcInfo(name: name, pid: pid)
 }
 
