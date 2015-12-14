6eb7786c96ddd12d87e6e4fa67a24492860de631
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-14 11:21:44 -0800

    add scripts/buildwinhelper.bat

--- scripts/buildwin.bat
+++ scripts/buildwin.bat
@@ -1,15 +1,6 @@
 @echo on
 
-godep go vet github.com/kjk/dbworkbench
-@IF ERRORLEVEL 1 goto Error
-
-call ./node_modules/.bin/gulp default
-@IF ERRORLEVEL 1 goto Error
-
-go run tools\build\cmd.go tools\build\gen_resources.go tools\build\main.go tools\build\s3.go tools\build\util.go tools\build\win.go -gen-resources
-@IF ERRORLEVEL 1 goto Error
-
-godep go build -tags embeded_resources -o dbherohelper.exe
+@call scripts\buildwinhelper.bat
 @IF ERRORLEVEL 1 goto Error
 
 go run tools\build\cmd.go tools\build\gen_resources.go tools\build\main.go tools\build\s3.go tools\build\util.go tools\build\win.go
