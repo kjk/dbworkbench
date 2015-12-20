37bff98f775b75519e6b11686e8903ba60b24756
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-19 16:04:00 -0800

    website: parsing and basic stats on usage.txt

--- website/usage_parse.go
+++ website/usage_parse.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -11,16 +13,13 @@ import (
 // parsing of usage.txt, stats based on that
 
 const (
+	// OsUnknown is default value for when os type is missing
+	OsUnknown = 0
 	// OsMac means running on Mac
 	OsMac = 1
 	// OsWindows running on Windows
 	OsWindows = 2
 )
-
-func testUsageParse() {
-	path := "usage-for-testing.go"
-	parseUsage(path)
-}
 
 // UsageUser describes statistics about users
 type UsageUser struct {
@@ -33,6 +32,7 @@ type UsageUser struct {
 	QueryCount        int
 	Os                int // 1 : mac, 2 : windows
 	OsVersion         string
+	Version           string
 }
 
 // records are separated by single empty line
@@ -48,6 +48,8 @@ func readUsageRecord(r *bufio.Reader) ([]string, error) {
 		if l == "" {
 			break
 		}
+		//fmt.Printf("l: '%s'\n", l)
+		res = append(res, l)
 	}
 	return res, err
 }
@@ -66,6 +68,84 @@ type UsageRecord struct {
 
 func parseUsageRecord(lines []string) (*UsageRecord, error) {
 	var res UsageRecord
+	// "key: value" lines
+	for len(lines) > 0 {
+		s := lines[0]
+		lines = lines[1:]
+		if strings.HasPrefix(s, "-----") {
+			break
+		}
+		parts := strings.SplitN(s, ":", 2)
+		if len(parts) != 2 {
+			LogErrorf("invalid line: '%s'\n", s)
+		}
+		val := strings.TrimSpace(parts[1])
+		switch parts[0] {
+		case "user":
+			res.Name = val
+		case "time":
+			secs, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				LogErrorf("invalid time value '%s' in line '%s', err: '%s'\n", val, s, err)
+			} else {
+				res.When = time.Unix(secs, 0)
+			}
+		case "ver_in_url":
+			// "ver" takes precedence
+			if res.Version == "" {
+				res.Version = val
+			}
+		case "ver":
+			// takes precedence over "ver_in_url"
+			res.Version = val
+		case "ostype":
+			if val == "mac" {
+				res.Os = OsMac
+			} else if val == "windows" {
+				res.Os = OsWindows
+			} else {
+				LogErrorf("unknown ostype '%s' in line '%s'\n", val, s)
+			}
+		case "osversion", "os":
+			// a bug of sorts: windows sends "os", mac sends "osversion"
+			res.OsVersion = val
+		case "serial":
+			// unique id on mac
+			res.ID = val
+		case "networkCardId":
+			// unique id on windows
+			res.ID = val
+		case "day", "ip", "machine", "net", "os_in_url":
+			// ignore
+		default:
+			LogErrorf("unknown key '%s' in line '%s'\n", parts[0], s)
+		}
+	}
+
+	// json lines
+	for len(lines) > 0 {
+		s := lines[0]
+		lines = lines[1:]
+		var v interface{}
+		err := json.Unmarshal([]byte(s), &v)
+		if err != nil {
+			LogErrorf("json.Unmarshal() of '%s' failed with '%s'\n")
+			continue
+		}
+		if m, ok := v.(map[string]interface{}); ok {
+			event := m["e"]
+			switch event {
+			case "do":
+				res.DatabaseOpenCount++
+			case "qe":
+				res.QueryCount++
+			default:
+				LogErrorf("Unknown event '%s' in '%s'\n", event, s)
+			}
+		} else {
+			LogErrorf("json.Unmarshal() produced value of type %T and not map[string]interface{}\n", v)
+		}
+	}
 	return &res, nil
 }
 
@@ -73,7 +153,7 @@ func parseUsage(path string) ([]*UsageUser, error) {
 	var res []*UsageUser
 	file, err := os.Open(path)
 	if err != nil {
-		LogErrorf("os.Open() failed with '%'s\n", err)
+		LogErrorf("os.Open() failed with '%s'\n", err)
 		return nil, err
 	}
 	defer file.Close()
@@ -100,11 +180,27 @@ func parseUsage(path string) ([]*UsageUser, error) {
 			user.ID = rec.ID
 			user.Name = rec.Name
 			user.Os = rec.Os
-			idToUser[rec.ID] = user
 			user.FirstSeen = rec.When
+
+			idToUser[rec.ID] = user
+			res = append(res, user)
 		}
-		// remember the latest version
-		user.OsVersion = rec.OsVersion
 		user.LastSeen = rec.When
+		user.DatabaseOpenCount += rec.DatabaseOpenCount
+		user.QueryCount += rec.QueryCount
+		// remember the latest versions
+		user.OsVersion = rec.OsVersion
+		user.Version = rec.Version
 	}
+}
+
+func testUsageParse() {
+	path := "usage-for-testing.txt"
+	timeStart := time.Now()
+	recs, err := parseUsage(path)
+	if err != nil {
+		LogErrorf("parseUsage() failed with '%s'\n")
+		return
+	}
+	LogInfof("%d users, time to parse: %s\n", len(recs), time.Since(timeStart))
 }
