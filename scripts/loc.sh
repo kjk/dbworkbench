33de92c57bdb4444e672a8bde03e13f05ab8bcce
thellimist <mufuyil@gmail.com>
2015-12-23 14:15:23 -0800

    more files added to line count script

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -5,8 +5,10 @@ set -o errexit
 set -o pipefail
 
 rm -f resources.go || true
+<<<<<<< HEAD
 echo && wc -l mac/dbHero/*.swift
 echo && wc -l win/dbhero/*.cs
 echo && wc -l s/*.html sass/main.scss
-echo && wc -l jsx/*.js*
+echo && wc -l jsx/*.js* jsx/lib/*.js* jsx/lib/reactable/*.js*
 echo && wc -l *.go website/*.go
+echo && wc -l scripts/*.*
