6e5908a923ad48f92a47bbcb1faad437a0ce97b3
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-12-26 21:31:46 -0800

    es6-ify AlertBar

--- jsx/AlertBar.jsx
+++ jsx/AlertBar.jsx
@@ -1,12 +1,7 @@
-/* jshint -W097,-W117 */
-'use strict';
+import React from 'react';
 
-const React = require('react');
-
-class AlertBar extends React.Component {
+export default class AlertBar extends React.Component {
   render() {
     return <div id="alert-bar">{this.props.errorMessage}</div>;
   }
 }
-
-module.exports = AlertBar;
