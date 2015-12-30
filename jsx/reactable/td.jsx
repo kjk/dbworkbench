4b1f73940df3acddb2268dc549ec5a6ad5e10fd1
thellimist <mufuyil@gmail.com>
2015-12-30 15:47:09 -0800

    when selecting cell textarea is focused but the cursor needs to be at end

--- jsx/reactable/td.jsx
+++ jsx/reactable/td.jsx
@@ -3,11 +3,37 @@ import { stringable, isReactComponent } from './utils.jsx';
 import { isUnsafe } from './unsafe.jsx';
 
 export class Td extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            isEditable: false,
+            value: 0,
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({isEditable: nextProps.isEditable});
+    }
+
+    handleOnFocus() {
+        console.log("handleOnFocus")
+        // TODO: somehow move the cursor to end
+    }
+
     handleClick(e){
         if (typeof this.props.onClick === 'function') {
             return this.props.onClick(e, this);
         }
     }
+
+    handleKeyDown(e) {
+        var ENTER = 13;
+        if( e.keyCode == ENTER ) {
+            console.log("Enter pressed", this)
+            this.setState({isEditable: false});
+        }
+    }
+
 
     render() {
         var tdProps = {
@@ -44,9 +70,20 @@ export class Td extends React.Component {
             }
         }
 
-        if (this.props.isEditable) {
+        if (this.state.isEditable) {
             // console.log("Editable Cell", this.props, tdProps)
-            return <td {...tdProps}><textarea value={this.props.children} onChange={this.props.onEdit}></textarea></td>;
+            return (
+                <td {...tdProps}>
+                    <textarea
+                        id="editable"
+                        autoFocus
+                        value={this.props.children}
+                        onFocus={this.handleOnFocus.bind(this)}
+                        onKeyDown={this.handleKeyDown.bind(this)}
+                        onChange={this.props.onEdit}>
+                    </textarea>
+                </td>
+            );
         }
 
         return <td {...tdProps}></td>;
