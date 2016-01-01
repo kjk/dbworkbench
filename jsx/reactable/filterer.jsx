2e13e1811906f2a2ac53a5d610eed0046c1376eb
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-01 14:23:09 -0800

    reformat rest of reactable

--- jsx/reactable/filterer.jsx
+++ jsx/reactable/filterer.jsx
@@ -10,11 +10,10 @@ export class Filterer extends React.Component {
     return (
       <input type="text"
         className="reactable-filter-input"
-        placeholder={this.props.placeholder}
-        value={this.props.value}
-        onKeyUp={this.onChange.bind(this)}
-        onChange={this.onChange.bind(this)}
-      />
-    );
+        placeholder={ this.props.placeholder }
+        value={ this.props.value }
+        onKeyUp={ this.onChange.bind(this) }
+        onChange={ this.onChange.bind(this) } />
+      );
   }
-};
+}
--- jsx/reactable/paginator.jsx
+++ jsx/reactable/paginator.jsx
@@ -1,131 +1,124 @@
 import React from 'react';
 
 function pageHref(num) {
-    return `#page-${num + 1}`;
+  return `#page-${num + 1}`;
 }
 
 export class Paginator extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  handlePrevious(e) {
+    e.preventDefault();
+    this.props.onPageChange(this.props.currentPage - 1);
+  }
+
+  handleNext(e) {
+    e.preventDefault();
+    this.props.onPageChange(this.props.currentPage + 1);
+  }
+
+  handlePageButton(page, e) {
+    e.preventDefault();
+    this.props.onPageChange(page);
+  }
+
+  handleSetCurrentPage(selectedPageNumber, e) {
+    if (this.props.numPages > selectedPageNumber || selectedPageNumber < 1) {
+      throw new Error(`invalid page number: ${selectedPageNumber}`);
+    }
+    this.props.onPageChange(number);
+  }
+
+  renderPrevious() {
+    if (this.props.currentPage <= 0) {
+      return (<a className='reactable-previous-page disabled'><i className="fa fa-chevron-left"></i></a>);
     }
 
-    handlePrevious(e) {
-        e.preventDefault();
-        this.props.onPageChange(this.props.currentPage - 1);
+    return (
+      <a className='reactable-previous-page' onClick={ this.handlePrevious.bind(this) } href={ pageHref(this.props.currentPage - 1) }><i className="fa fa-chevron-left"></i></a>
+      );
+  }
+
+  renderNext() {
+    if (this.props.currentPage >= this.props.numPages - 1) {
+      return (<a className='reactable-next-page disabled'><i className="fa fa-chevron-right"></i></a>);
     }
 
-    handleNext(e) {
-        e.preventDefault();
-        this.props.onPageChange(this.props.currentPage + 1);
+    return (
+      <a className='reactable-next-page' onClick={ this.handleNext.bind(this) } href={ pageHref(this.props.currentPage + 1) }><i className="fa fa-chevron-right"></i></a>
+      );
+  }
+
+  renderPageButton() {
+    return (<span className='reactable-page'>{ this.props.currentPage + 1 } / { this.props.numPages }</span>);
+  }
+
+  render() {
+    if (typeof this.props.numPages === 'undefined') {
+      throw new TypeError('Must pass a non-zero numPages argument to Paginator');
     }
 
-    handlePageButton(page, e) {
-        e.preventDefault();
-        this.props.onPageChange(page);
+    if (typeof this.props.currentPage === 'undefined') {
+      throw new TypeError('Must pass a currentPage argument to Paginator');
     }
+    var st = {
+      color: 'white'
+    };
 
-    handleSetCurrentPage(selectedPageNumber, e) {
-        if (this.props.numPages > selectedPageNumber || selectedPageNumber < 1) {
-            throw new Error(`invalid page number: ${selectedPageNumber}`);
-        }
-        this.props.onPageChange(number);
-    }
+    // let pageButtons = [];
+    // let pageButtonLimit = this.props.pageButtonLimit;
+    // let currentPage = this.props.currentPage;
+    // let numPages = this.props.numPages;
+    // let lowerHalf = Math.round( pageButtonLimit / 2 );
+    // let upperHalf = (pageButtonLimit - lowerHalf);
 
-    renderPrevious() {
-        if(this.props.currentPage <= 0) {
-            return (<a className='reactable-previous-page disabled'><i className="fa fa-chevron-left"></i></a>);
-        }
+    // for (let i = 0; i < this.props.numPages; i++) {
+    //     let showPageButton = false;
+    //     let pageNum = i;
+    //     let className = "reactable-page-button";
+    //     if (currentPage === i) {
+    //         className += " reactable-current-page";
+    //     }
+    //     pageButtons.push( this.renderPageButton(className, pageNum));
+    // }
 
-        return (
-            <a  className='reactable-previous-page'
-                onClick={this.handlePrevious.bind(this)}
-                href={pageHref(this.props.currentPage - 1)}>
-                <i className="fa fa-chevron-left"></i>
-           </a>
-        );
-    }
+    // if(currentPage - pageButtonLimit + lowerHalf > 0) {
+    //     if(currentPage > numPages - lowerHalf) {
+    //         pageButtons.splice(0, numPages - pageButtonLimit)
+    //     } else {
+    //         pageButtons.splice(0, currentPage - pageButtonLimit + lowerHalf);
+    //     }
+    // }
 
-    renderNext() {
-        if(this.props.currentPage >= this.props.numPages - 1) {
-            return (<a className='reactable-next-page disabled'><i className="fa fa-chevron-right"></i></a>);
-        }
+    // if((numPages - currentPage) > upperHalf) {
+    //     pageButtons.splice(pageButtonLimit, pageButtons.length - pageButtonLimit);
+    // }
 
-        return (
-            <a  className='reactable-next-page'
-                onClick={this.handleNext.bind(this)}
-                href={pageHref(this.props.currentPage + 1)}>
-                <i className="fa fa-chevron-right"></i>
-           </a>
-        );
-    }
+    // <tbody className="reactable-pagination">
+    //        <tr>
+    //            <td colSpan={this.props.colSpan}>
+    //                <div className="reactable-pagination-button-container">
+    //                {this.renderPrevious()}
+    //                {this.renderPageButton()}
+    //                {this.renderNext()}
+    //                </div>
+    //            </td>
+    //        </tr>
+    //    </tbody>
 
-    renderPageButton() {
-        return (<span className='reactable-page'>{this.props.currentPage + 1} / {this.props.numPages} </span>);
-    }
-
-    render() {
-        if (typeof this.props.numPages === 'undefined') {
-            throw new TypeError('Must pass a non-zero numPages argument to Paginator');
-        }
-
-        if (typeof this.props.currentPage === 'undefined') {
-            throw new TypeError('Must pass a currentPage argument to Paginator');
-        }
-        var st = {
-          color: 'white'
-        };
-        
-        // let pageButtons = [];
-        // let pageButtonLimit = this.props.pageButtonLimit;
-        // let currentPage = this.props.currentPage;
-        // let numPages = this.props.numPages;
-        // let lowerHalf = Math.round( pageButtonLimit / 2 );
-        // let upperHalf = (pageButtonLimit - lowerHalf);
-
-        // for (let i = 0; i < this.props.numPages; i++) {
-        //     let showPageButton = false;
-        //     let pageNum = i;
-        //     let className = "reactable-page-button";
-        //     if (currentPage === i) {
-        //         className += " reactable-current-page";
-        //     }
-        //     pageButtons.push( this.renderPageButton(className, pageNum));
-        // }
-
-        // if(currentPage - pageButtonLimit + lowerHalf > 0) {
-        //     if(currentPage > numPages - lowerHalf) {
-        //         pageButtons.splice(0, numPages - pageButtonLimit)
-        //     } else {
-        //         pageButtons.splice(0, currentPage - pageButtonLimit + lowerHalf);
-        //     }
-        // }
-
-        // if((numPages - currentPage) > upperHalf) {
-        //     pageButtons.splice(pageButtonLimit, pageButtons.length - pageButtonLimit);
-        // }
-
-         // <tbody className="reactable-pagination">
-         //        <tr>
-         //            <td colSpan={this.props.colSpan}>
-         //                <div className="reactable-pagination-button-container">
-         //                {this.renderPrevious()}
-         //                {this.renderPageButton()}
-         //                {this.renderNext()}
-         //                </div>
-         //            </td>
-         //        </tr>
-         //    </tbody>
-
-        return (
-            <div className="reactable-pagination">
-                <div className="reactable-pagination-button-container">
-                    {this.renderPrevious()}
-                    {this.renderPageButton()}
-                    {this.renderNext()}
-                    <span style={st}>{this.props.totalRowCount} rows</span>
-                </div>
-            </div>
-        );
-    }
-};
+    return (
+      <div className="reactable-pagination">
+        <div className="reactable-pagination-button-container">
+          { this.renderPrevious() }
+          { this.renderPageButton() }
+          { this.renderNext() }
+          <span style={ st }>{ this.props.totalRowCount } rows</span>
+        </div>
+      </div>
+      );
+  }
+}
+;
 
