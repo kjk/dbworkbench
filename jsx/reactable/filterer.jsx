e113ea5c316227b1a0f0f696c21a756d80f423b8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-01 11:51:55 -0800

    simplify Filterer

--- jsx/reactable/filterer.jsx
+++ jsx/reactable/filterer.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class FiltererInput extends React.Component {
-    onChange() {
-        this.props.onFilter(ReactDOM.findDOMNode(this).value);
-    }
-
-    render() {
-        return (
-            <input type="text"
-                className="reactable-filter-input"
-                placeholder={this.props.placeholder}
-                value={this.props.value}
-                onKeyUp={this.onChange.bind(this)}
-                onChange={this.onChange.bind(this)} />
-        );
-    }
-};
-
 export class Filterer extends React.Component {
-    render() {
-        return (
-            <FiltererInput onFilter={this.props.onFilter}
-                value={this.props.value}
-                placeholder={this.props.placeholder}/>
-        );
-    }
-};
+  onChange() {
+    this.props.onFilter(ReactDOM.findDOMNode(this).value);
+  }
 
+  render() {
+    return (
+      <input type="text"
+        className="reactable-filter-input"
+        placeholder={this.props.placeholder}
+        value={this.props.value}
+        onKeyUp={this.onChange.bind(this)}
+        onChange={this.onChange.bind(this)}
+      />
+    );
+  }
+};
--- jsx/reactable/tr.jsx
+++ jsx/reactable/tr.jsx
@@ -3,42 +3,42 @@ import { Td } from './td.jsx';
 import { toArray, filterPropsFrom } from './utils.jsx';
 
 export class Tr extends React.Component {
-    render() {
-        var children = React.Children.toArray();
+  render() {
+    var children = React.Children.toArray();
 
-        if (
-            this.props.data &&
-                this.props.columns &&
-                    typeof this.props.columns.map === 'function'
-        ) {
-            if (typeof(children.concat) === 'undefined') { console.log(children); }
+    if (
+      this.props.data &&
+        this.props.columns &&
+          typeof this.props.columns.map === 'function'
+    ) {
+      if (typeof(children.concat) === 'undefined') { console.log(children); }
 
-            children = children.concat(this.props.columns.map(function(column, i) {
-                if (this.props.data.hasOwnProperty(column.key)) {
-                    var value = this.props.data[column.key];
-                    var props = {};
+      children = children.concat(this.props.columns.map(function(column, i) {
+        if (this.props.data.hasOwnProperty(column.key)) {
+          var value = this.props.data[column.key];
+          var props = {};
 
-                    if (
-                        typeof(value) !== 'undefined' &&
-                            value !== null &&
-                                value.__reactableMeta === true
-                    ) {
-                        props = value.props;
-                        value = value.value;
-                    }
+          if (
+            typeof(value) !== 'undefined' &&
+              value !== null &&
+                value.__reactableMeta === true
+          ) {
+            props = value.props;
+            value = value.value;
+          }
 
-                    return <Td column={column} key={column.key} {...props}>{value}</Td>;
-                } else {
-                    return <Td column={column} key={column.key} />;
-                }
-            }.bind(this)));
+          return <Td column={column} key={column.key} {...props}>{value}</Td>;
+        } else {
+          return <Td column={column} key={column.key} />;
         }
-
-        // Manually transfer props
-        var props = filterPropsFrom(this.props);
-
-        return React.DOM.tr(props, children);
+      }.bind(this)));
     }
+
+    // Manually transfer props
+    var props = filterPropsFrom(this.props);
+
+    return React.DOM.tr(props, children);
+  }
 };
 
 Tr.childNode = Td;
