10ce0d00198523fdbca579d655c2d2d5d2868ed2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-01-02 23:15:16 -0800

    also js format tests

--- jsx/tests/sort.js
+++ jsx/tests/sort.js
@@ -1,14 +1,14 @@
 var assert = require('assert');
 
-function multiply(x, y) {
-  return x*y;
-};
+require('./../../s/fake_data_1.js');
 
-describe("multiply", function () {
-  it("returns the correct multiplied value", function () {
+function multiply(x, y) {
+  return x * y;
+}
+
+describe('multiply', function() {
+  it('returns the correct multiplied value', function() {
     var res = multiply(2, 4);
     assert.equal(res, 8);
   });
 });
-
-
--- scripts/jsfmt.sh
+++ scripts/jsfmt.sh
@@ -4,4 +4,5 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-./node_modules/.bin/esformatter -i jsx/reactable/*.jsx jsx/*js*
+./node_modules/.bin/esformatter -i jsx/reactable/*.jsx jsx/*js* jsx/tests/*.js
+
